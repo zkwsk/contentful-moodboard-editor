061d24a8617efb46efc64b106818761975481b39
Zaki Wasik <zakiwa@gmail.com>
2021-09-10 19:45:46 +0200

    Make sure assets are loaded initially

--- src/containers/MoodboardEditorContainer/MoodboardEditorContainer.tsx
+++ src/containers/MoodboardEditorContainer/MoodboardEditorContainer.tsx
@@ -95,57 +95,61 @@ const MoodboardEditorContainer = ({sdk}: MoodboardEditorContainerProps) => {
     setFieldData(field.getValue());
   };
 
+  const updateStateFromAssetField = () => {
+    if (!sdk) {
+      return;
+    }
+    // Grab references to the images
+    const imageLinks: Link[] = sdk.entry.fields[ASSETS_FIELD_ID]?.getValue();
+
+    if (!imageLinks) {
+      return;
+    }
+    // Extract IDs
+    const imageIds = imageLinks?.map((link) => link.sys?.id);
+
+    // Get assets via the SDK
+    const assetPromises = imageIds?.map((id) => sdk.space.getAsset(id));
+
+    Promise.all(assetPromises).then((data) => {
+      const filteredData = data.filter((asset: { [prop: string]: any }) => {
+        return !objectIsEmpty(asset.fields);
+      });
+
+      const parsedImages = filteredData.map(
+        (asset: { [prop: string]: any }): Asset => {
+          const type = asset.fields.file['en-US'].contentType;
+          const filename = asset.fields.file['en-US'].fileName;
+          const title = asset.fields.title['en-US'];
+          const width = asset.fields.file['en-US'].details.image?.width || 1920;
+          const height =
+            asset.fields.file['en-US'].details.image?.height || 1080;
+
+          return {
+            id: asset.sys.id,
+            filename,
+            title,
+            type,
+            width,
+            height,
+            element: {
+              description: asset.fields.description?.['en-US'],
+              url: asset.fields.file['en-US'].url,
+            },
+          };
+        },
+      );
+      setAssets(parsedImages);
+    });
+  };
+
   useEffect(() => {
     // Listen for changes on the asset field.
-    const detach = assetField.onValueChanged((value) => {
-      if (!sdk) {
-        return;
-      }
-      // Grab references to the images
-      const imageLinks: Link[] = sdk.entry.fields[ASSETS_FIELD_ID]?.getValue();
-
-      if (!imageLinks) {
-        return;
-      }
-      // Extract IDs
-      const imageIds = imageLinks?.map((link) => link.sys?.id);
-
-      // Get assets via the SDK
-      const assetPromises = imageIds?.map((id) => sdk.space.getAsset(id));
-
-      Promise.all(assetPromises).then((data) => {
-        const filteredData = data.filter((asset: { [prop: string]: any }) => {
-          return !objectIsEmpty(asset.fields);
-        });
-
-        const parsedImages = filteredData.map(
-          (asset: { [prop: string]: any }): Asset => {
-            const type = asset.fields.file['en-US'].contentType;
-            const filename = asset.fields.file['en-US'].fileName;
-            const title = asset.fields.title['en-US'];
-            const width =
-              asset.fields.file['en-US'].details.image?.width || 1920;
-            const height =
-              asset.fields.file['en-US'].details.image?.height || 1080;
-
-            return {
-              id: asset.sys.id,
-              filename,
-              title,
-              type,
-              width,
-              height,
-              element: {
-                description: asset.fields.description?.['en-US'],
-                url: asset.fields.file['en-US'].url,
-              },
-            };
-          },
-        );
-        setAssets(parsedImages);
-      });
+    const detach = assetField.onValueChanged(() => {
+      updateStateFromAssetField();
     });
     return () => detach();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [assetField, sdk]);
 
   // useEffect(()=> {
